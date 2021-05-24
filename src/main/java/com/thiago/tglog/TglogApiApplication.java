a280bb3811ce1c93c9d4a4aa9d47b8c65a7b6961
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-05-24 17:40:12 -0300

    Projeto criado

---
+++ src/main/java/com/thiago/tglog/TglogApiApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.tglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TglogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TglogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/tglog/TglogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.tglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TglogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
