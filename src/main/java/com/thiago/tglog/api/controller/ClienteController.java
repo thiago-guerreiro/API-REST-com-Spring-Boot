60d5375e238cb4f1bc7903bc94844e1bfb0f4cfc
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-05-26 14:15:22 -0300

    Feito o mapeamento objeto relacional com Jakarta Persistence

--- src/main/java/com/thiago/tglog/api/controller/ClienteController.java
+++ src/main/java/com/thiago/tglog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.thiago.tglog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,13 @@ import com.thiago.tglog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("11 99999-9999");
-		cliente1.setEmail("joao@emailcom");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("11 99999-8888");
-		cliente2.setEmail("maria@emailcom");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 	
 }
