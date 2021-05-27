63c3124f8477f3fa3b4a1a4a5bc1431183b5561f
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-05-27 16:04:23 -0300

    Interface ClienteRepository e persistência dos dados com Spring Data JPA

--- src/main/java/com/thiago/tglog/api/controller/ClienteController.java
+++ src/main/java/com/thiago/tglog/api/controller/ClienteController.java
@@ -2,24 +2,23 @@ package com.thiago.tglog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thiago.tglog.domain.model.Cliente;
+import com.thiago.tglog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
-	
+	private ClienteRepository clienteRepository;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findAll();
 	}
 	
 }
---
+++ src/main/java/com/thiago/tglog/domain/repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.thiago.tglog.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thiago.tglog.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+
+}
