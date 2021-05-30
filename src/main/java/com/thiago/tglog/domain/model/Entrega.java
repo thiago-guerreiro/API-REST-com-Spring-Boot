d1a548febf0daf17efdec7488202b20e6e0e5979
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-05-30 17:10:26 -0300

    Isolado Domain Model do Representation Model

--- src/main/java/com/thiago/tglog/domain/model/Entrega.java
+++ src/main/java/com/thiago/tglog/domain/model/Entrega.java
@@ -1,7 +1,7 @@
 package com.thiago.tglog.domain.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -18,7 +18,6 @@ import javax.validation.groups.Default;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
-
 import com.thiago.tglog.domain.ValidationGroups;
 
 import lombok.EqualsAndHashCode;
@@ -55,9 +54,9 @@ public class Entrega {
 	private StatusEntrega status;
 	
 	@JsonProperty(access = Access.READ_ONLY)
-	private LocalDateTime dataPedido;
+	private OffsetDateTime dataPedido;
 	
 	@JsonProperty(access = Access.READ_ONLY)
-	private LocalDateTime dataFinalizacao;
+	private OffsetDateTime dataFinalizacao;
 
 }
