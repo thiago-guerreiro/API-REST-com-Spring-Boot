847e14f5edc43a0f4994e9682597304b546759be
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-05-31 17:37:36 -0300

    ModelMapper

---
+++ src/main/java/com/thiago/tglog/api/model/input/ClienteIdInput.java
@@ -0,0 +1,15 @@
+package com.thiago.tglog.api.model.input;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClienteIdInput {
+	
+	@NotNull
+	private Long id;
+
+}
